a00e4880873edf3a433883d19ac2adb5fc54724d
maneonkar <newuser3@company.com>
2020-10-27 01:56:58 +0530

    initial commit

--- jobs/views/employee/views.py
+++ jobs/views/employee/views.py
@@ -24,45 +24,6 @@ class AppliedJobs(LoginRequiredMixin, IsUserEmployee, View):
             return redirect('jobs:landing')
 
 
-# class ApplyJobView(LoginRequiredMixin, CreateView):
-#     model = Applicant
-#     form_class = ApplyJobForm
-#     slug_field = 'job_id'
-#     slug_url_kwarg = 'job_id'
-#
-#     @method_decorator(login_required(login_url=reverse_lazy('accounts:login')))
-#     def dispatch(self, request, *args, **kwargs):
-#         return super().dispatch(self.request, *args, **kwargs)
-#
-#     def post(self, request, *args, **kwargs):
-#         form = self.get_form()
-#         if form.is_valid():
-#             messages.info(self.request, 'Successfully applied for the job!')
-#             return self.form_valid(form)
-#         else:
-#             return HttpResponseRedirect(reverse_lazy('jobs:home'))
-#
-#     def get_success_url(self):
-#         return reverse_lazy('jobs:job-details', kwargs={'job_id': self.kwargs['job_id']})
-#
-#     # def get_form_kwargs(self):
-#     #     kwargs = super(ApplyJobView, self).get_form_kwargs()
-#     #     print(kwargs)
-#     #     kwargs['job'] = 1
-#     #     return kwargs
-#
-#     def form_valid(self, form):
-#         # check if user already applied
-#         applicant = Applicant.objects.filter(user_id=self.request.user.id, job_id=self.kwargs['job_id'])
-#         if applicant:
-#             messages.info(self.request, 'You already applied for this job')
-#             return HttpResponseRedirect(self.get_success_url())
-#         # save applicant
-#         form.instance.user = self.request.user
-#         form.save()
-#         return super().form_valid(form)
-
-
 class ApplyJobView(LoginRequiredMixin, IsUserEmployee, CreateView):
     model = Applicant
 
